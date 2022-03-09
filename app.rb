cc0baea19443834537b85c0cfa68a1ce203a47a6
erik-greenblomroos-nti-johanneberg <74712714+erik-greenblomroos-nti-johanneberg@users.noreply.github.com>
2022-03-09 11:27:25 +0100

    auction container

--- app.rb
+++ app.rb
@@ -10,5 +10,16 @@ slim(:login)
 end
 
 get('/auction') do
-slim(:auction)
+    # db = SQLite3::Database.new("db/database.db")
+    # db.results_as_hash = true
+    # result = db.execute("SELECT * FROM NFT")
+    # p result
+    # slim(:"auction/index",locals:{NFT:result})
+    redirect('/auction/index')
 end
+
+get('/auction/index') do
+    slim(:"auction/index")
+end
+
+
